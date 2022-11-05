edf0b2ef543e5d82988e4a76309d66e034650f8d
genda <gendalf.mesa@gmail.com>
2022-11-05 13:32:31 +0200

    Begin

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        int x = 5;
+        System.out.println("Hello world " + x + " Maria");
+        System.out.println();
+        System.out.println("Hello world " + x + " Maria");
+        // d - число s - символи, рядок, текст
+    }
+}
